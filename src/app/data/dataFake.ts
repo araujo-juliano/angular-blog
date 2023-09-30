864a6c1b3df68e2fcb80d5022754b59277a836ec
Juliano Araujo <julianoaraujo.dev@gmail.com>
2023-09-30 10:35:26 -0300

    Criação do Content component

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "Morte de Satoru Gojo",
+    "description": "Novo cápitulo de Jujutsu Kaisen revela morte inesperada do personagem Satoru Gojo",
+    "photoCover": "https://sm.ign.com/ign_br/screenshot/default/imagem-2023-07-21-151541208_yebw.jpg"
+  },
+  {
+    "id": "2",
+    "title": "Netflix anuncia continuação do Live Action de One Piece",
+    "description": "Sucesso surpreende diretores da Netflix ue já anunciam segunda temporada",
+    "photoCover": "https://jornaldaparaiba.com.br/wp-content/uploads/2023/01/onepiece.webp"
+  },
+  {
+    "id": "3",
+    "title": "Retorno do anime Bleach",
+    "description": "Bleach retorna trazendo a hitória da grande guerra",
+    "photoCover": "https://s2-techtudo.glbimg.com/xZERWZGLFAALKU2yriDvoZit5gU=/1200x/smart/filters:cover():strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/T/x/3cBYkkRpipAoMFoQAHLQ/bleach1.jpg"
+  },
+  {
+    "id": "4",
+    "title": "Lançamento da segunda temporada de Loki",
+    "description": "Segunda temporada da série Loki chega nas telas",
+    "photoCover": "https://pop.proddigital.com.br/wp-content/uploads/sites/8/2022/07/loki-season-2-confirmed.jpg"
+  }
+]
